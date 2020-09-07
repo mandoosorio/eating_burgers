b05e6f6280eee908ba05b64dd2513ee71d442707
Euphory <syntherev0@gmail.com>
2020-09-07 13:43:59 -0700

    adding initial files required for MVC design pattern

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('grilled cheese');
+INSERT INTO burgers (burger_name) VALUES ('pb&j');
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('chicken sandwich', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('double bacon cheese burger', true);
+INSERT INTO burgers (burger_name) VALUES ('protein burger');
